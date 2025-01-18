a10b4cdff85d8c0eab2ec8709d2c059a3c22fe3b
Akshay Kumar <140506882+akshay-8285@users.noreply.github.com>
2025-01-18 17:46:52 +0530

    Check

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -7,9 +7,15 @@ using System.Collections;
 public class UiManager : MonoBehaviour
 {
     [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private Button pauseButton , resumeButton;
+    
     void Start()
     {
         gameOverPanel.SetActive(false);
+        pauseButton.onClick.AddListener(OnClickPauseButton);
+        resumeButton.onClick.AddListener(OnClickResumeButton);
+        pauseButton.gameObject.SetActive(true);
         
         
     }
@@ -23,5 +29,19 @@ public class UiManager : MonoBehaviour
     {
         yield return new WaitForSeconds(2f);
         gameOverPanel.SetActive(true);
+    }
+    public void  OnClickPauseButton()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0;
+        pauseButton.gameObject.SetActive(false);
+        
+    }
+    public void OnClickResumeButton()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1;
+        pauseButton.gameObject.SetActive(true);
+
     }
 }
